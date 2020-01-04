0589be39a5eb83a6d5fcca3a48ed568990ce34e7
tsubasa1122 <nakanot@open8.com>
2020-01-04 17:49:18 +0900

    [fix]api以下はformat jsonにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, format: 'json' do
     mount_devise_token_auth_for 'User', at: 'auth'
-    resources :tasks, except: %i[new]
+    resources :tasks, except: %i[new show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
