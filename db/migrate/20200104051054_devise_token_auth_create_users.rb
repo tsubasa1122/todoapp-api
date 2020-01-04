03d8330c1a801dbd5d648ca0e29800ac8752c451
tsubasa1122 <nakanot@open8.com>
2020-01-04 14:25:04 +0900

    [add]userテーブル

--- db/migrate/20200104051054_devise_token_auth_create_users.rb
+++ db/migrate/20200104051054_devise_token_auth_create_users.rb
@@ -17,12 +17,17 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       ## Rememberable
       t.datetime :remember_created_at
 
+      ## Trackable
+      t.integer :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
       ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
@@ -30,8 +35,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
 
       ## User Info
       t.string :name
-      t.string :nickname
-      t.string :image
+      # t.string :nickname
+      # t.string :image
       t.string :email
 
       ## Tokens
@@ -43,7 +48,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
 end
