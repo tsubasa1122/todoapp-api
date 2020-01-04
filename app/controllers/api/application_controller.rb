0e2556f4b0125d0afa16c89bb449ef069c18b599
tsubasa1122 <nakanot@open8.com>
2020-01-04 17:18:24 +0900

    [fix]api配下にcontrollerを作成

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,4 @@
+class Api::ApplicationController < ActionController::API
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+end
--- app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::API
-        skip_before_action :verify_authenticity_token, if: :devise_controller?
-        include DeviseTokenAuth::Concerns::SetUserByToken
-end
