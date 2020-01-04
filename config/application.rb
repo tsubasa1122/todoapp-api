2b8d0d7f1535fbcd42dab1f1733019f838d0751e
tsubasa1122 <nakanot@open8.com>
2020-01-04 16:11:26 +0900

    helper、stylesheet、test_frameworkは自動で生成しないようにする

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,11 @@ module TodoappApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.test_framework false
+    end
     config.api_only = true
   end
 end
