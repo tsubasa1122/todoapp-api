ece54c569c40c98929fc08d66924262d1949d320
tsubasa1122 <nakanot@open8.com>
2020-01-04 17:50:35 +0900

    [fix]ApplicationControllerがないとActionTextでエラーが出る

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,4 +1,4 @@
-class Api::ApplicationController < ActionController::API
+class Api::ApplicationController < ApplicationController
   skip_before_action :verify_authenticity_token, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
 end
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,27 @@
 class Api::TasksController < Api::ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:user_id, :title, :content, :expires_at, :completion_flg)
+  end
 end
