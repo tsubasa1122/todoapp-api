854f2626bd0382bc249b4eea9b53227b6bb1da92
tsubasa1122 <nakanot@open8.com>
2020-01-11 12:57:24 +0900

    taskのAPI修正

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,27 +1,46 @@
 class Api::TasksController < Api::ApplicationController
+  before_action :set_task, only: %i[show edit update]
+  
   def index
     @tasks = Task.all
   end
 
-  def create
+  def show
+  end
 
+  def create
+    task = current_user.tasks.new(task_params)
+    if task.save
+      render 'success'
+    else
+      render 'error'
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if task.update(task_params)
+      render 'success'
+    else
+      render 'error'
+    end
   end
 
   def delete
-
+    task = task.find(params[:id])
+    task.destory
+    render 'success'
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:user_id, :title, :content, :expires_at, :completion_flg)
+    params.require(:task).permit(:user_id, :title, :content, :expires_at, :completion_flg, tags_attributes:[:id, :name])
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
